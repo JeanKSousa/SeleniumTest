c67ecd13ab7148e39399a8463b46ee1d44f6af25
jeank <jeank@JEANK65>
2019-12-12 09:56:29 -0500

    Cambio 2

--- SeleniumDemoUI3/SeleniumDemoUI/Login.cs
+++ SeleniumDemoUI3/SeleniumDemoUI/Login.cs
@@ -1,9 +1,12 @@
-﻿
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Chrome;
-
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
 using OpenQA.Selenium.Remote;
 
 
@@ -12,10 +15,33 @@ namespace SeleniumDemoUI
     [TestClass]
     public class Login
     {
-        private RemoteWebDriver _browserDriver;
+        //private RemoteWebDriver _browserDriver;
         [TestMethod]
-
         public void Inicializacion()
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("headless");
+            using (var _driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), chromeOptions))
+            {
+                
+                _driver.Navigate().GoToUrl("http://localhost:3500/user/login");
+                _driver.Manage().Window.Maximize();
+                //var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(50));
+
+                IWebElement query = _driver.FindElement(By.Name("username"));
+                query.SendKeys("admin");
+
+                IWebElement query2 = _driver.FindElement(By.Name("password"));
+                query2.SendKeys("admin");
+
+                IWebElement query3 = _driver.FindElement(By.XPath(".//*[@id='sub']"));
+                query3.Click();
+
+                Assert.AreEqual(_driver.Title, "The Shop");
+
+            }
+        }
+        /*
         {
             //arranque
             _browserDriver = new ChromeDriver();
@@ -31,6 +57,6 @@ namespace SeleniumDemoUI
             Assert.IsTrue(_browserDriver.PageSource.Contains("admin"));
             Assert.IsTrue(_browserDriver.PageSource.Contains("Salir"));
             Assert.IsTrue(_browserDriver.PageSource.Contains("Bienvenido a la e-shop!"));
-        }
+        }*/
     }
 }
